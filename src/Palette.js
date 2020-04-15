260c511dc449fe38230571e0ee143f8a8aa7fc13
Anton Aroche <anton.aroche@gmail.com>
2020-04-15 19:34:44 -0400

    Completed single palette component with header, footer and go back button

--- src/Palette.js
+++ src/Palette.js
@@ -3,6 +3,7 @@ import "rc-slider/assets/index.css";
 import "./Palette.css";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
+import PaletteFooter from "./PaletteFooter";
 
 class Palette extends Component {
   state = {
@@ -37,12 +38,10 @@ class Palette extends Component {
           level={level}
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
+          showSlider
         />
         <div className="Palette-colors">{renderBoxes}</div>
-        <footer className="Palette-footer">
-          {paletteName}
-          <span className="emoji">{emoji}</span>
-        </footer>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
+import PaletteFooter from "./PaletteFooter";
 
 class SingleColorPalette extends Component {
   constructor(props) {
     super(props);
     this._shades = this.gatherShades(this.props.palette, this.props.colorId);
   }
+  state = { format: "hex" };
+
   gatherShades(palette, filterColorId) {
     let shades = [];
     const allColors = palette.colors;
@@ -16,20 +21,33 @@ class SingleColorPalette extends Component {
     }
     return shades.slice(1);
   }
+
+  changeFormat = (format) => {
+    this.setState({ format: format });
+  };
+
   render() {
+    const { format } = this.state;
+    const { paletteName, emoji, id } = this.props.palette;
     const colorBoxes = this._shades.map((color) => (
       <ColorBox
-        key={color.id}
-        color={color.hex}
+        key={color.name}
+        color={color[format]}
         name={color.name}
         showLink={false}
       />
     ));
 
     return (
-      <div className="Palette">
-        <h1>Single Palette</h1>
-        <div className="Palette-colors">{colorBoxes}</div>
+      <div className="SingleColorPalette Palette">
+        <Navbar showSlider={false} handleChange={this.changeFormat} />
+        <div className="Palette-colors">
+          {colorBoxes}
+          <div className="go-back ColorBox">
+            <Link to={`/palette/${id}`} className="back-button">GO BACK</Link>
+          </div>
+        </div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
