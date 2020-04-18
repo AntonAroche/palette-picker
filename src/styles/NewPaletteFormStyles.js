3ffa849c2e6926b03f902e40e04a4bb63f0c19d0
Anton Aroche <anton.aroche@gmail.com>
2020-04-17 23:18:17 -0400

    Improved nav bar styles

--- src/styles/NewPaletteFormStyles.js
+++ src/styles/NewPaletteFormStyles.js
@@ -1,46 +1,44 @@
 import { DRAWER_WIDTH } from "../constants";
 const drawerWidth = DRAWER_WIDTH;
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    display: "flex"
-  },
-  hide: {
-    display: "none"
+    display: "flex",
   },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    height: "100vh"
+    height: "100vh",
   },
   drawerPaper: {
     width: drawerWidth,
     display: "flex",
-    alignItems: "center"
+    alignItems: "center",
   },
   drawerHeader: {
     display: "flex",
     alignItems: "center",
+    width: "100%",
     padding: "0 8px",
     ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
+    justifyContent: "flex-end",
   },
   content: {
     flexGrow: 1,
     height: "calc(100vh - 64px)",
-    padding: theme.spacing.unit * 3,
+    padding: 0,
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
+      duration: theme.transitions.duration.leavingScreen,
     }),
-    marginLeft: -drawerWidth
+    marginLeft: -drawerWidth,
   },
   contentShift: {
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
+      duration: theme.transitions.duration.enteringScreen,
     }),
-    marginLeft: 0
+    marginLeft: 0,
   },
   container: {
     width: "90%",
@@ -48,14 +46,14 @@ const styles = theme => ({
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   buttons: {
-    width: "100%"
+    width: "100%",
   },
   button: {
-    width: "50%"
-  }
+    width: "50%",
+  },
 });
 
 export default styles;
