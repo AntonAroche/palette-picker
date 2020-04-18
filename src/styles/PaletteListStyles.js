9674e4b13904d837be747ec8baefad119da1d8e1
Anton Aroche <anton.aroche@gmail.com>
2020-04-18 12:32:58 -0400

    Added background and made palette picker page responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,10 +1,22 @@
+import sizes from "./sizes";
+import bg from "./bg.svg";
+
 export default {
   root: {
-    backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    background: `url(${bg}) no-repeat center center fixed`,
+    webkitBackgroundSize: "cover",
+    mozBackgroundSize: "cover",
+    oBackgroundSize: "cover",
+    backgroundSize: "cover",
+    overflow: "scroll",
+  },
+  heading: {
+    fontSize: "2rem",
   },
   container: {
     width: "50%",
@@ -12,6 +24,9 @@ export default {
     alignItems: "flex-start",
     flexDirection: "column",
     flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "70%",
+    },
   },
   nav: {
     display: "flex",
@@ -28,6 +43,14 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%",
+    gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+      gridGap: "2.0rem",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1.5rem",
+    },
   },
 };
