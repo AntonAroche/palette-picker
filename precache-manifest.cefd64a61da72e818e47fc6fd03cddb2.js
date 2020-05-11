453630478f21a3ad40426311c42abeaf7790e9cc
Anton Aroche <anton.aroche@gmail.com>
2020-05-11 10:01:29 -0400

    Updates

---
+++ precache-manifest.cefd64a61da72e818e47fc6fd03cddb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72e1b31cf7f8545adb1178686bda20b",
+    "url": "/palette-picker/index.html"
+  },
+  {
+    "revision": "258b26e0f18541711b8a",
+    "url": "/palette-picker/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "83389798bf2e66e7e01b",
+    "url": "/palette-picker/static/css/main.5029bbcc.chunk.css"
+  },
+  {
+    "revision": "258b26e0f18541711b8a",
+    "url": "/palette-picker/static/js/2.8cda67f9.chunk.js"
+  },
+  {
+    "revision": "aa6c723bcb7bf9a4c546b6aa137fdb1e",
+    "url": "/palette-picker/static/js/2.8cda67f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83389798bf2e66e7e01b",
+    "url": "/palette-picker/static/js/main.166812e4.chunk.js"
+  },
+  {
+    "revision": "4a36d647431cce2b4b0f",
+    "url": "/palette-picker/static/js/runtime-main.ebb428f6.js"
+  },
+  {
+    "revision": "61edda80c85da224a79082501f94c3f9",
+    "url": "/palette-picker/static/media/bg.61edda80.svg"
+  }
+]);
